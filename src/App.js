9fc925f391b3385733f77560c9ad5092ede80479
Altora Mclean <altoramclean@gmail.com>
2021-11-09 17:00:24 -0500

    working through app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Form from './Form';
+import TeamMember from './TeamMember';
+
 import './App.css';
 
+const initialValue = {
+  username: '',
+  email: '',
+  role:'',
+}
+
+
+
+
+
+
+
+
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [members, setMembers] = useState ([]);
+  const [formValues, setFormValues] = useState(initialValue);
+
+const updateForm = (input1, input2) => {
+    setFormValues({...formValues, [input1]: input2})
+  }
+  
+
+const submitForm = () => {
+  const newMember = {
+
+  username: formValues.username.trim(),
+  email: formValues.email.trim(),
+  role: formValues.role.trim()
+  }
+}
+
+
+return (
+  <div className="App">
+    
+  </div>
+);
 }
 
 export default App;
